3cbf932294aee1f57793ab32bdf1cc2640956b38
Donald Robertson <donaldarobertson89@gmail.com>
2016-12-15 22:01:49 +0000

    Complete rewrite

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
+
 'use strict'
 
-var path = require('path')
-var fs = require('fs')
-var realPath = fs.realpathSync(__dirname)
-var script = path.join(realPath, 'lib', 'pino-gelf.js')
+const program = require('commander')
+const version = require('./package.json').version
+const pinoGelf = require('./lib/pinoGelf')
 
-require(script.toString())
+function parseCustomFields (val) {
+  if (val === undefined) return []
+  if (val.includes(',')) return val.split(',')
+  return [ val ]
+}
+
+program
+  .version(version)
+
+program
+  .command('log')
+  .description('Run Pino-GELF')
+  .option('-h, --host [host]', 'Graylog Host')
+  .option('-p, --port [port]', 'Graylog Port', parseInt)
+  .option('-m, --max-chunk-size [maxChunkSize]', 'Graylog Input Maximum Chunk Size', parseInt)
+  .option('-e, --use-express-middleware-preset')
+  .option('-c, --specify-custom-fields [keyList]', 'Comma Separated List of Custom Keys', parseCustomFields)
+  .option('-v, --verbose', 'Output GELF to console')
+  .action(function () {
+    const opts = {
+      customKeys: this.specifyCustomFields || [],
+      host: this.host || '127.0.0.1',
+      maxChunkSize: this.maxChunkSize || 1420,
+      port: this.port || 12201,
+      useExpressMiddlewarePreset: this.useExpressMiddlewarePreset || false,
+      verbose: this.verbose || false
+    }
+
+    pinoGelf(opts)
+  })
+
+program
+  .parse(process.argv)
